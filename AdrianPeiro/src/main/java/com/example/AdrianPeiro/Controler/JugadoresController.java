f75061becfaf0b91e9e078df56d18b3b86188ae6
serra <adrpeiser@alu.edu.gva.es>
2025-02-09 12:06:38 +0100

    se tiene que acabar

--- AdrianPeiro/src/main/java/com/example/AdrianPeiro/Controler/JugadoresController.java
+++ AdrianPeiro/src/main/java/com/example/AdrianPeiro/Controler/JugadoresController.java
@@ -1,12 +1,15 @@
 package com.example.AdrianPeiro.Controler;
 
+import com.example.AdrianPeiro.Model.Jugadores;
 import com.example.AdrianPeiro.Model.JugadoresRepository;
+import com.example.AdrianPeiro.Model.ListaJugadores;
 import com.example.AdrianPeiro.Model.ListaJugadoresRepository;
+import com.example.AdrianPeiro.Model.Torneos;
 import com.example.AdrianPeiro.Model.TorneosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
@@ -21,31 +24,24 @@ public class JugadoresController {
     @Autowired
     TorneosRepository torneosRepository;
 
-
     @GetMapping("/jugadores")
-    public String getJugadores(Model model) {
-        model.addAttribute("Jugador", jugadoresRepository.findAll());
+    public String verJugadores(Model model) {
+        Iterable<Jugadores> jugadores = jugadoresRepository.findAll();
+        model.addAttribute("jugadores", jugadores);
         return "jugadores";
     }
 
     @GetMapping("/listas")
-    public String getListas(Model model) {
-        model.addAttribute("ListaJugador", listaJugadoresRepository.findAll());
+    public String verListas(Model model) {
+        Iterable<ListaJugadores> listaJugadores = listaJugadoresRepository.findAll();
+        model.addAttribute("listaJugadores", listaJugadores);
         return "listaJugadores";
     }
 
-
     @GetMapping("/torneos")
-    public String getTorneos(Model model) {
-        model.addAttribute("Torneo", torneosRepository.findAll());
+    public String verTorneos(Model model) {
+        Iterable<Torneos> torneos = torneosRepository.findAll();
+        model.addAttribute("torneo", torneos);
         return "torneo";
     }
-
-
-
-
-
-
-
 }
-
